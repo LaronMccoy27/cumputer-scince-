735feda6c30bc35508ccf3b7f1a7b2190bd7493c
LaronMccoy27 <827736@mtstma.org>
2024-09-26 14:43:30 -0500

    data conversion assighnment

--- unit 2 - functions and variables/data_coversion.py
+++ unit 2 - functions and variables/data_coversion.py
@@ -8,7 +8,26 @@ g = '10110111'  #byte -> int
 h = "2.54"      #string -> float
 i = 100         #int -> float
 j = 10.0        #float -> int
-k = 254         #int -> 
-a-str = (a)
+k = 254         #int -> bin
+a_str = (a)
 print(a_str)
-
+float_str = str(b)
+print(str(b))
+c_str = int(c)
+print(int(c))
+d_str = bool(d)
+print(int(d))
+e_str = str(e)
+print(int(e))
+f_str = str(f)
+print(int(f))
+g_int = int(g)
+print(g_int)
+h_float = float(h)
+print(h_float)
+i_float = float(i)
+print(i_foat)
+j_int = int(j)
+print(j_int)
+k_bin = bin(k)
+print(k_bin)
