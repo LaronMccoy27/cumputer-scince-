df520449ac6f9e4a8f28d3e7a03c94a129b78f93
LaronMccoy27 <827736@mtstma.org>
2024-09-30 14:29:03 -0500

    top5movies

---
+++ assignments/SimpleMadLib.py
@@ -0,0 +1,8 @@
+#once upon of time , in a blue house 
+#there was a big red dog.
+#every day , the big red dog would eat my homework happily.
+#one day , a white truck appeared 
+#and blew the dog away 
+#the dog was scared and sad.
+blue_house = input("Please enter a shelter.\n>")
+red_dog = 
---
+++ unit 2 - functions and variables/top5movies.py
@@ -0,0 +1,10 @@
+
+def top_movies(a,b,c,d,e):
+    print("laron top five moives")
+    print("1 + a")
+    print("2 + b")
+    print("3 + c")
+    print("4 + d")
+    print("5 + e")
+
+my_movies("daddy's little girl", "hulk", "spider-man", "micheal myer", "US" )
