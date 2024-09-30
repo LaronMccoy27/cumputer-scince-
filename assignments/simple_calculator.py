30df18bb9ce70e5e4d80f5661f58b5d976cc7564
LaronMccoy27 <827736@mtstma.org>
2024-09-30 15:08:56 -0500

    simplecalculator

---
+++ assignments/simple_calculator.py
@@ -0,0 +1,15 @@
+def add (x,y):
+    print(x + y)
+add(10, 1)
+
+def subtract(x,y):
+    print(x - y)
+subtract(10, 1)
+
+def multiply(x,y):
+    print(x * y)
+multiply(10, 1)
+
+def divied(x,y):
+    print(x / y)    
+divied(10, 1)
