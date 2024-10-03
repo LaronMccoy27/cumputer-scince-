89b89649cbd3f000475896ef17da0c1c3b3b1bcd
LaronMccoy27 <827736@mtstma.org>
2024-10-03 15:10:52 -0500

    test

---
+++ test/unit2test15point.py
@@ -0,0 +1,4 @@
+color = input(" please enter you favroite color")
+animal = input(" please enter your favorite animal")
+movie = input("pleaase enter your favorite movie") 
+print("the first word was color" first_word , + "the second word was animal" second_word, + "the thrid word was movie" thrid_word )
---
+++ test/unit2test7points.py
@@ -0,0 +1,10 @@
+def add_three(x,y,z):
+    print(x + y + z )
+
+x = input("please enter a number\n)")
+y = input("please enter a number\n)") 
+z = input("please enter a number\n)")
+
+num = float(num)
+dec = float(dec)
+add = float(num + dec)
