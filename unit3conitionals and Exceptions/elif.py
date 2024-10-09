5ee1c494443cbc1fb48954cf555f912551ece61e
LaronMccoy27 <827736@mtstma.org>
2024-10-09 15:17:56 -0500

    hurricane

---
+++ unit3conitionals and Exceptions/elif.py
@@ -0,0 +1,28 @@
+# the if statement has two buddles
+# the first little buddy is the else statement 
+
+x = 10
+
+if x > 0:    # not every if need an else 
+    print("x is a postive number")
+
+# the second little buddy is the elif (else if)
+
+else:     # else always needs a if
+    print("x is a negtive number")
+
+else:     # else always needs and if 
+    print("x is zero")
+
+
+light = input(" what color is the light?\n>")
+
+
+ light.lower() == "green"
+    print("STOP IF SAFE")
+
+
+elif light.lower() == "yellow"
+    print("STOP")
+
+
---
+++ unit3conitionals and Exceptions/hurricane_classifier.py
@@ -0,0 +1,21 @@
+speed = input("how fast is a hurricane\n>")
+
+speed = int(speed)
+if speed < 74:
+    print("tropical storm")
+
+elif speed < 96:
+    print("category 1")
+
+elif speed < 111:
+    print("category 2") 
+
+elif speed < 130:
+    print("category 4")
+
+elif speed <= 157:
+    print("category 5")
+
+
+
+    
